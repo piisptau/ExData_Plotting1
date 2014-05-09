6ceef59f3af1469cc43b8b859948ef702734c502
Tauno Piispanen <tauno.piispanen@tieto.com>
2014-05-09 16:15:18 +0300

    Comments added

--- plot4.r
+++ plot4.r
@@ -1,12 +1,26 @@
+#
+# Exploratory Data Analysis Assigment 1 Plot 4 code
+#
+
+# Read source data file
+
 hhpow <- read.csv("C:/Users/pt/Downloads/household_power_consumption.txt", sep=";", header=T, stringsAsFactors=F, na.strings="?")
+
+# Change date and time columns to suitable format
 
 hhpow$Time <- strptime(paste(hhpow$Date, hhpow$Time), format='%d/%m/%Y %H:%M:%S')
 hhpow$Date = as.Date(hhpow$Date, format="%d/%m/%Y")
 
+# Select a subset containing only dates 2007-02-01 and 2007-02-02
+
 hhc = subset(hhpow, Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"))
+
+# Set locale so that the plot x axis weekday labels show up in english
 
 Sys.setlocale(category = "LC_ALL", locale = "C")
 par(mar=c(4.1,4.1,3.1,3.1))
+
+# Print the plot with four panes to the screen
 
 par(mfrow=c(2,2))
 
@@ -24,6 +38,7 @@ par(new=F)
 
 plot(hhc$Time, hhc$Global_reactive_power, type="l", xlab="datetime", ylab = "Global_reactive_power")
 
+# Print the same plot to a PNG file
 
 png("plot4.png", width = 480, height = 480)
 par(mfrow=c(2,2))
